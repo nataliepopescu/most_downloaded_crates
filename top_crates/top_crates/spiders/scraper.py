153f40edb690b4471227ad61490ff46a936e6c9c
Natalie Popescu <nataliepopescu@gmail.com>
2020-11-30 21:43:36 -0700

    add support for creating list out of bencher rev deps

--- top_crates/top_crates/spiders/scraper.py
+++ top_crates/top_crates/spiders/scraper.py
@@ -7,8 +7,8 @@ import subprocess
 
 class CratesSpider(scrapy.Spider):
     name = 'top'
-    per_page = 50
-    total_page = 998
+    per_page = 10 #50
+    total_page = 1#4 #998
     filename = "CrateList.json"
     namelist = "sorted_crates.py"
     count = 0
@@ -16,7 +16,8 @@ class CratesSpider(scrapy.Spider):
     crates = []
 
     def start_requests(self):
-        url = 'https://crates.io/api/v1/crates?page={page}&per_page={per_page}&sort=downloads'
+        self.url = 'https://crates.io/api/v1/crates/bencher/reverse_dependencies?page={page}&per_page={per_page}'
+        #self.url = 'https://crates.io/api/v1/crates?page={page}&per_page={per_page}&sort=downloads'
         def write_time():
             secs = subprocess.run(["date", "+%s"], stdout=subprocess.PIPE, text=True)
             nanos = subprocess.run(["date", "+%N"], stdout=subprocess.PIPE, text=True)
@@ -28,23 +29,40 @@ class CratesSpider(scrapy.Spider):
         write_time()
         for page in range(self.total_page):
             yield Request.from_curl(
-                "curl " + url.format(page=page+1, per_page=self.per_page),
+                "curl " + self.url.format(page=page+1, per_page=self.per_page),
                 callback=self.parse)
 
 
     def parse(self, response):
         data = json.loads(response.body.decode('utf-8'))
 
-        for crate in data['crates']:
-            self.count += 1
-            if 'name' not in crate or 'newest_version' not in crate:
-                print("Error: invalid json for crate " + crate['id'])
+        if 'reverse_dependencies' in self.url: 
+            if 'versions' not in data:
+                print("Error: invalid json")
                 return None
-            item = {"Package": {
-                    "name": crate['name'], 
-                    "version": crate['newest_version'],
-                    "downloads": crate['downloads']}}
-            self.crates.append(item)
+
+            for crate in data['versions']:
+                self.count += 1
+                item = {"Package": {
+                        "name": crate['crate'],
+                        "version": crate['num'],
+                        "downloads": crate['downloads']}}
+                self.crates.append(item)
+        else:
+            if 'crates' not in data:    
+                print("Error: invalid json")
+                return None
+
+            for crate in data['crates']:
+                self.count += 1
+                if 'name' not in crate or 'newest_version' not in crate:
+                    print("Error: invalid json for crate " + crate['id'])
+                    return None
+                item = {"Package": {
+                        "name": crate['name'], 
+                        "version": crate['newest_version'],
+                        "downloads": crate['downloads']}}
+                self.crates.append(item)
 
         # only sort towards end
         if self.count > (self.per_page * (self.total_page - 1)):
